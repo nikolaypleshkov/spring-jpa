305b3854a264abaad322bcb78fb67e86774603f9
nikolaypleshkov <icelol@abv.bg>
2021-04-19 17:30:07 +0300

    Update Controller.java

--- spring-jpa/src/main/java/com/pleshkov/springjpa/Controller/Controller.java
+++ spring-jpa/src/main/java/com/pleshkov/springjpa/Controller/Controller.java
@@ -1,15 +1,50 @@
 package com.pleshkov.springjpa.Controller;
 
+import com.pleshkov.springjpa.Student.ConfirmationToken;
+import com.pleshkov.springjpa.Student.ConfirmationTokenService;
 import com.pleshkov.springjpa.Student.Service;
 import com.pleshkov.springjpa.Entity.Student;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-@RestController
-@RequestMapping(path = "web/api/student")
+
+//@RestController
+//@RequestMapping(path = "web/api/student")
+
+@org.springframework.stereotype.Controller
+@AllArgsConstructor
 public class Controller {
+    private final Service service;
+    private final ConfirmationTokenService confirmationTokenService;
+
+    @GetMapping("/sign-in")
+    String signIn(){
+        return "sign-in";
+    }
+
+    @GetMapping("/sign-up")
+    String signUpPage(Student student){
+        return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    String signUp(Student student){
+        service.signUpStudent(student);
+        return "redirect:/sign-in";
+    }
+
+    @PostMapping("/sign-up/confirm")
+    String confirmEmail(@RequestParam("token") String token){
+        Optional<ConfirmationToken> optionalConfirmationToken = confirmationTokenService.findConfirmationTokenByToken(token);
+        optionalConfirmationToken.ifPresent(service::confirmStudent);
+
+        return "redirect:/sign-in";
+    }
+    /*
     private final Service service;
 
     @Autowired
@@ -38,5 +73,9 @@ public class Controller {
                     @RequestParam(required = false) String email){
         service.put(id,name,email);
     }
+
+ */
+
+
 
 }
