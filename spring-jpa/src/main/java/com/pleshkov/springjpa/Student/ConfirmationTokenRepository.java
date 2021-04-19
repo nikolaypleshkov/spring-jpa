69f57e5eb4fbffaa2d28f0a070b628be05cdc61d
nikolaypleshkov <icelol@abv.bg>
2021-04-19 17:30:28 +0300

    Create ConfirmationTokenRepository.java

---
+++ spring-jpa/src/main/java/com/pleshkov/springjpa/Student/ConfirmationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.pleshkov.springjpa.Student;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, Long> {
+    Optional<ConfirmationToken> findConfirmationTokenByConfirmationToken(String token);
+}
