bd18d3df3391ead70d4cd4d3cce3ea3122beddcd
nikolaypleshkov <icelol@abv.bg>
2021-04-19 17:30:11 +0300

    Update StudentDAO.java

--- spring-jpa/src/main/java/com/pleshkov/springjpa/DAO/StudentDAO.java
+++ spring-jpa/src/main/java/com/pleshkov/springjpa/DAO/StudentDAO.java
@@ -3,13 +3,12 @@ package com.pleshkov.springjpa.DAO;
 import com.pleshkov.springjpa.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface StudentDAO extends JpaRepository<Student, Long> {
-
-    @Query("SELECT st FROM Student st WHERE st.email = ?1")
+public interface StudentDAO extends CrudRepository<Student, Long> {
     Optional<Student> findStudentByEmail(String email);
 }
